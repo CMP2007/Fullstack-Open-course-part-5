e1b33244683b2b1a6291fd0362a70055a396395c
PucheCarlos <puchecarlos00@gmail.com>
2026-03-30 10:24:32 -0400

    ejercice 5.18

--- ejercicio_5.1/E2E-Test/tests/note_app.spec.js
+++ ejercicio_5.1/E2E-Test/tests/note_app.spec.js
@@ -10,4 +10,39 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
   })
+
+  describe('Testing login', () => {
+    beforeEach(async ({page, request}) => {
+      await request.post('/api/testing/reset')
+      await request.post('/api/users', {
+        data: {
+          name: 'test',
+          username: 'test',
+          password: 'test'
+        }
+      })
+    })
+    test('You can log in successfully', async ({page}) => {
+      await expect(page.getByLabel('username')).toBeVisible()
+      await page.getByRole('textbox').first().fill('test')
+      await page.getByRole('textbox').last().fill('test')
+      await page.getByRole('button', { name: 'login' }).click() 
+
+      await expect(page.getByText('test logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await expect(page.getByLabel('username')).toBeVisible()
+      await page.getByLabel('username').fill('test')
+      await page.getByLabel('password').fill('error')
+      await page.getByRole('button', { name: 'login' }).click() 
+
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('wrong username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+      await expect(page.getByText('test logged in')).not.toBeVisible()
+    })
+  })
 })
